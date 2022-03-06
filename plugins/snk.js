41b0d2dc3382f34d57476220848b947e1e606dbe
Leo <97941567+leoganteng123456789@users.noreply.github.com>
2022-03-06 17:38:41 +0700

    Create snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -4,30 +4,29 @@ let handler = async(m, { conn }) => {
 Kebijakan Privasi, Syarat Ketentuan dan Peraturan R-Txzy Bot
 
 Kebijakan Privasi
-1. R-Txzy tidak akan merekam data riwayat chat user.
-2. R-Txzy tidak akan menyebarkan nomor users.
-3. R-Txzy tidak akan menyimpan media yang dikirimkan oleh users.
-4. R-Txzy tidak akan menyalah gunakan data data users.
-5. Owner R-Txzy berhak melihat data riwayat chat users.
-6. Owner R-Txzy berhak melihat status users.
-7. Owner R-Txzy dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. YoXzy tidak akan merekam data riwayat chat user.
+2. YoXzy tidak akan menyebarkan nomor users.
+3. YoXzy tidak akan menyimpan media yang dikirimkan oleh users.
+4. YoXzy tidak akan menyalah gunakan data data users.
+5. Owner YoXzy berhak melihat data riwayat chat users.
+6. Owner YoXzy berhak melihat status users.
+7. Owner YoXzy dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan R-Txzy Bot
+Peraturan YoXzy Bot
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
 4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
 
-Syarat Ketentuan R-Txyz Bot
+Syarat Ketentuan YoXzy Bot
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. R-Txzy dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. R-Txzy *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. R-Txzy akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. R-Txzy bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. YoXzy dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. YoXzy *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. YoXzy akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. YoXzy bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
-- AlyaXd
-- TioXd
+- YoXzy
 
 Peraturan: 1 Februari 2022
 `.trim()
